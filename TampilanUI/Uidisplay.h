c749ee6a5d6eb0b3bf611c488bba75f93bf82aa0
faRkkyyy <fakhrie.rizdan.tif23@polban.ac.id>
2024-05-21 11:06:26 +0700

    commit ui

---
+++ TampilanUI/Uidisplay.h
@@ -0,0 +1,12 @@
+#ifndef UIDISPLAY_H
+#define UIDISPLAY_H
+
+void clearScreen();
+void setColor(int color);
+void resetColor();
+void welcomePage();
+bool registerAdmin();
+bool adminLogin();
+void adminOptions();
+
+#endif // UIDISPLAY_H
