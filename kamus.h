c12c2f8679584a682a869932f6d16e287a1a461f
Jufri23 <muhammad.haikal.tif23@polba.ac.id>
2024-05-21 10:45:24 +0700

    First AVL

---
+++ kamus.h
@@ -0,0 +1,30 @@
+#ifndef KAMUS_H
+#define KAMUS_H
+
+#include <iostream>
+#include <string>
+#include <fstream> // tambahkan ini untuk ofstream
+using namespace std;
+
+struct Node {
+    string kata;
+    string arti;
+    Node* kiri;
+    Node* kanan;
+    int height;
+};
+
+Node* sisipkanAVL(Node* root, string kata, string arti);
+Node* hapusNode(Node* root, string kata);
+void tampilkanSemuaKata(Node* root, ofstream& file);
+Node* cari(Node* root, string kata);
+void tampilkanArti(Node* root, string kata);
+int getHeight(Node* root);
+int getBalanceFactor(Node* root);
+Node* rotateKiri(Node* root);
+Node* rotateKanan(Node* root);
+Node* rotate(Node* root);
+Node* successor(Node* root);
+Node* predecessor(Node* root);
+
+#endif
